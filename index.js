fc10d5c2ccb27151b444f8dc62f1fe3984db1a88
TechShivam02 <shivamtech2024@gmail.com>
2023-04-13 14:19:03 +0530

    crud update

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ let express = require("express");
 const app = express();
 
 
+const bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({extended:true}));
+
+
 
 let dbs = require('./database');   // importing the dbs 
 
@@ -9,11 +13,8 @@ let dbs = require('./database');   // importing the dbs
 // path to view , set the view ending and body parser
 
 var path = require("path");
-const { error } = require("console");
-const { rmSync } = require("fs");
 
 
-app.use(express.urlencoded({extended:false}));
 app.set("views" , path.join(__dirname,'view'));
 app.set('view engine' , 'ejs');
 
@@ -44,13 +45,19 @@ app.get("/listuser" , function(req,res){
 
     let sql = "select * from user ";
 
+    let msg = "";
+    if(req.query['msg'] != ""){
+        msg = req.query['msg'];
+    }
+
+
     dbs.query(sql , function(error , result,field){
         if(error){
             res.redirect("/");
         }
 
         else{
-            res.render("userlistview" , {data:result});
+            res.render("userlistview" , {data:result , msg:msg});
         }
     })
     
@@ -88,9 +95,6 @@ app.post("/addusersubmit" , function(req,res){
     let sql = "insert into user(name , email , pass , adress , mobile , dob , gender) values('"+name+"' , '"+email+"' ,  '"+pass+"'  , '"+adress+"' , "+mobile+" , '"+dob+"'  , '"+gen+"' )"; 
 
 
-
-
-
     let msg="";
 
 
@@ -121,10 +125,11 @@ app.post("/addusersubmit" , function(req,res){
 
 
 app.get('/edituser' , function(req,res){
+    
+    
     let sql = "select * from user where id="+req.query['id'];
 
-
-    res.write(sql);
+    // res.write(sql);
 
     dbs.query(sql,function(err,result , field){
         if(err){
@@ -132,13 +137,64 @@ app.get('/edituser' , function(req,res){
         }
 
         else{
-            console.log(result);
+            res.render("edituser_view",{data:result});
         }
+
     });
 
-    res.end();
-
+    // res.end();
 })
+
+
+
+app.post("/editusersubmit", function(req,res){
+
+    let id = req.body.id;
+
+    let name = req.body.name;   
+    let email = req.body.Email; 
+    
+    let adress = req.body.adress;
+    let mobile = req.body.mobile;
+ 
+
+    let sqlupdate = "update user set name='"+name+"',email='"+email+"',adress='"+adress+"',mobile="+mobile+" where id="+id;
+
+    dbs.query(sqlupdate,function(err,result,fields){
+
+        if(err){
+            res.redirect("/listuser?msg=can not update the data");
+            console.log("id : " + id);
+            console.log(err);
+
+        }   
+
+        else{
+            res.redirect("/listuser?msg=data updated");
+        }
+
+    })
+});
+
+
+
+
+app.get("/deluser" , function(req,res){
+
+    let sql_del = "delete from user where id="+req.query['id'];
+
+    dbs.query(sql_del,function(err,result,field){
+        if(err){
+            res.redirect("/listuser?msg=unable to del the user");
+        }   
+
+        else{
+            res.redirect("/listuser?msg=user deleted");
+        }
+    })
+
+});
+
 
 app.listen(8080);
 console.log("server running at port 8080");
